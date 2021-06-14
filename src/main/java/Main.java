90a674b4a96de4736ea73db22c075c1a275ecbc0
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2021-06-14 17:12:04 +0200

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
+import com.goterl.lazysodium.Sodium;
+import com.goterl.lazysodium.SodiumJava;
 import com.sun.jna.Native;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("JNA Version: " + Native.VERSION);
+
+        Native.register(Sodium.class, "sodium");
+        Native.register(SodiumJava.class, "sodium");
     }
 }
